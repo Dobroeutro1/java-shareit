81ec303440a471e3935a6cba6095dda04e96ecaf
denkovalsky <artyom.noskov.1998@gmail.com>
2023-09-20 19:32:24 +0500

    Запрос вещи. Тесты.

---
+++ src/main/java/ru/practicum/shareit/exeption/PageableParamsException.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.exeption;
+
+public class PageableParamsException extends RuntimeException {
+
+    public PageableParamsException(String message) {
+        super(message);
+    }
+
+}
